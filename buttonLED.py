3f934688226db48df2d169f4482b01b4f22c883b
Roger Dodger <lpaddikt@gmail.com>
2018-06-19 19:15:53 -0700

    Added LED and Clock

--- buttonLED.py
+++ buttonLED.py
@@ -1,5 +1,16 @@
-from nanpy import (ArduinoApi, SerialManager)
+from nanpy import (ArduinoApi, SerialManager, Lcd)
 from time import sleep
+import datetime
+
+lcd = Lcd([8, 9, 4, 5, 6, 7], [16, 2])
+status = 't'
+while status == 't':
+    lcd.clear()
+    lcd.printString(datetime.datetime.now())
+    
+    status = input("What status do you want: ")
+lcd.clear()
+lcd.printString('Dead!!!')
 
 ledPin = 13
 ledState = True
@@ -17,6 +28,7 @@ try:
         if ledState:
             a.digitalWrite(ledPin, a.LOW)
             ledState = False
+            lcd.clear()
             print("LED OFF")
             sleep(1)
         else:
@@ -26,3 +38,6 @@ try:
             sleep(1)
 except:
     a.digitalWrite(ledPin, a.LOW)
+
+
+
